b409b66786f8ed6ce6e2dd430d42b9db07e0fc2c
Damyan Peykov <dppeykov@gmail.com>
2019-10-30 09:57:10 +0100

    End of video 67
    The basic routing is done

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -53,8 +53,8 @@ export default class Drirectory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
