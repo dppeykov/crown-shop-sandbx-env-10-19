2ec7be1b1b5d44af29327aa2dbab3db250e8b3d9
Damyan Peykov <dppeykov@gmail.com>
2019-10-31 11:57:27 +0100

    End of video 88
    Added the functionality to connect to the firestore, get a snapshot to check if the user is saved or not and if not - to save the user to the database

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -21,8 +21,9 @@ export default class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
+      //this.setState({ currentUser: user });
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
   appId: "1:213428739604:web:6aaf2f334f3f9bb6bd9101"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // checks if we have an user: if userAuth == null (False) = no user, ! will revert it and the function will return; if user userAuth == object (True) - ! will revert it and we will execute the False branch
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`/users/${userAuth.uid}`); //document reference
+  const snapShot = await userRef.get();
+
+  //if the user does NOT exist - we will create it in the DB
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 // Initialize Firebase - docs https://firebase.google.com/docs/web/setup?authuser=0
 // sign-in docs: https://firebase.google.com/docs/auth/web/google-signin?authuser=0
 firebase.initializeApp(config);
