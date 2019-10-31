46ca3f7ef219ef70891ab9adf71315a3a7eb8d10
Damyan Peykov <dppeykov@gmail.com>
2019-10-31 12:11:20 +0100

    End of video 89
    The app can track the state of the current user and saves the info for the user + it can communicate with the firebase firestore db

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,20 @@ export default class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-      //this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
